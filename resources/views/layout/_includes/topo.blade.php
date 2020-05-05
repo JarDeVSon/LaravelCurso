214d67206ebcc23b0fe83dbc87fde182456da4df
Jardeson Santos <github.com/JarDeVSon>
2020-05-05 02:04:02 -0300

    Site web concluido

--- resources/views/layout/_includes/topo.blade.php
+++ resources/views/layout/_includes/topo.blade.php
@@ -12,25 +12,36 @@
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
-<body>
-    <header>
-        <nav>
-            <div class="nav-wrapper blue">
-              <a href="#!" class="brand-logo">Curso de Laravel</a>
-              <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-              <ul class="right hide-on-med-and-down">
-                <li><a href="/">Home</a></li>
-                <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+    <body>
+        <header>
+            <nav>
+                <div class="nav-wrapper blue">
+                <div class="container">
+                <a href="#!" class="brand-logo">LaravelCurso</a>
+                <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="/">Home</a></li>
+                        @if (Auth::guest())
+                        <li><a href="{{route('site.login')}}">Login</a></li>
+                        @else
+                        <li><a href="#">{{Auth::user()->name}}</a></li>
+                        <li><a href="{{route('site.login.sair')}}">Sair</a></li>
+                        @endif
+                    </ul>
 
-              </ul>
-            </div>
+
+                    <ul class="sidenav" id="mobile">
+                            <li><a href="/">Home</a></li>
+                            @if (Auth::guest())
+                            <li><a href="{{route('site.login')}}">Login</a></li>
+                            @else
+                            <li><a href="#">{{Auth::user()->name}}</a></li>
+                            <li><a href="{{route('site.login.sair')}}">Sair</a></li>
+                            @endif
+
+                    </ul>
+                </div>
+              </div>
           </nav>
-
-          <ul class="sidenav" id="mobile">
-            <li><a href="/">Home</a></li>
-            <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
-
-          </ul>
-
-    </header>
-</body>
+      </header>
+ </body>
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,19 @@ Route::post('/contato/', ['uses'=>'ContatoController@criar']);
 Route::put('/contato/', ['uses'=>'ContatoController@editar']);
 // Rotas do Curso
 
+Route::group(['middleware' => 'auth'], function () {
+    
+    Route::get('/admin/cursos', ['as'=> 'admin.cursos', 'uses'=> 'AdminCursoController@index']);
+    Route::get('/admin/cursos/adicionar', ['as'=> 'admin.cursos.adicionar', 'uses'=> 'AdminCursoController@adicionar']);
+    Route::post('/admin/cursos/salvar', ['as'=> 'admin.cursos.salvar', 'uses'=> 'AdminCursoController@salvar']);
+    Route::get('/admin/cursos/editar/{id}', ['as'=> 'admin.cursos.editar', 'uses'=> 'AdminCursoController@editar']);
+    Route::put('/admin/cursos/atualizar/{id}', ['as'=> 'admin.cursos.atualizar', 'uses'=> 'AdminCursoController@atualizar']);
+    Route::get('/admin/cursos/deletar/{id}', ['as'=> 'admin.cursos.deletar', 'uses'=> 'AdminCursoController@deletar']);
 
+});
 
 Route::get('/', ['as'=> 'site.home', 'uses'=> 'Site\HomeController@index']);
+Route::get('/login', ['as'=> 'site.login', 'uses'=> 'Site\LoginController@index']);
+Route::post('/login/entrar', ['as'=> 'site.login.entrar', 'uses'=> 'Site\LoginController@entrar']);
+Route::get('/login/sair', ['as'=> 'site.login.sair', 'uses'=> 'Site\LoginController@sair']);
 
-Route::get('/admin/cursos', ['as'=> 'admin.cursos', 'uses'=> 'AdminCursoController@index']);
-Route::get('/admin/cursos/adicionar', ['as'=> 'admin.cursos.adicionar', 'uses'=> 'AdminCursoController@adicionar']);
-Route::post('/admin/cursos/salvar', ['as'=> 'admin.cursos.salvar', 'uses'=> 'AdminCursoController@salvar']);
-Route::get('/admin/cursos/editar/{id}', ['as'=> 'admin.cursos.editar', 'uses'=> 'AdminCursoController@editar']);
-Route::put('/admin/cursos/atualizar/{id}', ['as'=> 'admin.cursos.atualizar', 'uses'=> 'AdminCursoController@atualizar']);
-Route::get('/admin/cursos/deletar/{id}', ['as'=> 'admin.cursos.deletar', 'uses'=> 'AdminCursoController@deletar']);
